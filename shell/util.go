80e0f6857883fcca34b0496452cbcd2e5bb81578
Jeff Lindsay <progrium@gmail.com>
2023-11-22 16:21:39 -0600

    shell: overall refactor to simplify, plus routing mechanism for stdin. closes #3 closes #4 closes #15

--- shell/util.go
+++ shell/util.go
@@ -2,116 +2,19 @@ package main
 
 import (
 	"bytes"
-	"context"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
 	"reflect"
 	"strings"
+	"sync"
 	"unsafe"
 
-	"golang.org/x/term"
-	"tractor.dev/toolkit-go/engine/fs"
 	"tractor.dev/wanix/kernel/proc/exec"
 )
 
-// todo: these should all be able to be removed in place of os.Getwd, os.Getenv
-var shellEnv = map[string]string{}
-var exeEnv = map[string]string{}
-
-type CmdType int
-
-const (
-	CmdIsScript CmdType = iota
-	CmdIsSourceDir
-	CmdIsWasm
-)
-
-type CmdSearchResult struct {
-	CmdType
-	found bool
-	path  string
-}
-
-// Search environment and/or input path to find an executable
-func searchForCommand(path string) (result CmdSearchResult) {
-	dfs := os.DirFS("/")
-
-	if result = getCmdByPath(dfs, path); result.found {
-		return
-	}
-
-	// start searching cmd directories
-	// TODO: search for cmd directories instead of just these predefined ones.
-	// TODO: search shell env PATH for executable.
-
-	exeName := filepath.Base(path)
-	if result = getCmdByPath(dfs, filepath.Join("/cmd", exeName)); result.found {
-		return
-	}
-	if result = getCmdByPath(dfs, filepath.Join("/sys/cmd", exeName)); result.found {
-		return
-	}
-	if result = getCmdByPath(dfs, filepath.Join("/sys/bin", exeName)); result.found {
-		return
-	}
-
-	return CmdSearchResult{found: false}
-}
-
-// Checks for file at `path`, `path.wasm`, `path.sh`, and a directory at `path`.
-func getCmdByPath(iofs fs.FS, path string) CmdSearchResult {
-	path = absPath(path)
-
-	isfile, err := fileExists(iofs, unixToFsPath(path))
-	if err == nil && isfile {
-		var ct CmdType
-		if strings.HasSuffix(path, ".sh") {
-			ct = CmdIsScript
-		} else {
-			ct = CmdIsWasm
-		}
-
-		return CmdSearchResult{
-			found:   true,
-			path:    path,
-			CmdType: ct,
-		}
-	}
-
-	wasm_path := strings.Join([]string{path, ".wasm"}, "")
-	isfile, err = fileExists(iofs, unixToFsPath(wasm_path))
-	if err == nil && isfile {
-		return CmdSearchResult{
-			found:   true,
-			path:    wasm_path,
-			CmdType: CmdIsWasm,
-		}
-	}
-
-	script_path := strings.Join([]string{path, ".sh"}, "")
-	isfile, err = fileExists(iofs, unixToFsPath(script_path))
-	if err == nil && isfile {
-		return CmdSearchResult{
-			found:   true,
-			path:    script_path,
-			CmdType: CmdIsScript,
-		}
-	}
-
-	if isDir, _, err := dirExists(iofs, unixToFsPath(path)); err == nil && isDir {
-		return CmdSearchResult{
-			found:   true,
-			path:    path,
-			CmdType: CmdIsSourceDir,
-		}
-	}
-
-	return CmdSearchResult{found: false}
-}
-
-func buildCmdSource(path string) (exePath string, err error) {
+func buildCmdSource(path string) (wasmPath string, err error) {
 	cmd := exec.Command("build", "-output", "/sys/bin/", path)
 	_, err = cmd.Run()
 	return filepath.Join("/sys/bin", filepath.Base(path)+".wasm"), err
@@ -119,38 +22,32 @@ func buildCmdSource(path string) (exePath string, err error) {
 
 var WASM_MAGIC = []byte{0, 'a', 's', 'm'}
 
-func isWasmFile(name string) (bool, error) {
-	if f, err := os.Open(name); err != nil {
-		return false, err
-	} else {
-		magic := make([]byte, 4)
-		if _, err := f.Read(magic); err != nil {
-			return false, err
-		}
-
-		return bytes.Equal(magic, WASM_MAGIC), nil
+func isWasmFile(name string) bool {
+	f, err := os.Open(name)
+	if err != nil {
+		return false
 	}
+	defer f.Close()
+	magic := make([]byte, 4)
+	if _, err := f.Read(magic); err != nil {
+		return false
+	}
+	return bytes.Equal(magic, WASM_MAGIC)
 }
 
-func parseEnvVars(t *term.Terminal, args []string) (ok bool, overrideEnv map[string]string, argsRest []string) {
+func parseEnvArgs(args []string, env map[string]string) (rest []string, err error) {
 	for i, arg := range args {
 		name, value, found := strings.Cut(arg, "=")
 		if !found {
-			argsRest = args[i:]
+			rest = args[i:]
 			break
 		}
 		if name == "" {
-			io.WriteString(t, fmt.Sprintf("invalid environment variable (%d): missing variable name\n", i))
-			return false, overrideEnv, args
+			return rest, fmt.Errorf("invalid variable at arg %d", i)
 		}
-		// delay making the override until we absolutely need it. Ensures it's null if empty
-		if overrideEnv == nil {
-			overrideEnv = make(map[string]string)
-		}
-		overrideEnv[name] = value
+		env[name] = value
 	}
-
-	return true, overrideEnv, argsRest
+	return rest, nil
 }
 
 func unpackArray[S ~[]E, E any](s S) []any {
@@ -169,27 +66,12 @@ func unpackMap(m map[string]string) map[string]any {
 	return r
 }
 
-func unpackMap2(m map[string]string) []string {
+func packEnv(m map[string]string) []string {
 	r := make([]string, len(m))
 	for k, v := range m {
 		r = append(r, strings.Join([]string{k, v}, "="))
 	}
 	return r
-}
-
-type cancelableReader struct {
-	r   io.Reader
-	ctx context.Context
-}
-
-func (cr cancelableReader) Read(p []byte) (n int, err error) {
-	select {
-	case <-cr.ctx.Done():
-		return 0, cr.ctx.Err()
-	default:
-		n, err := cr.r.Read(p)
-		return n, err
-	}
 }
 
 // Unix absolute path. Returns cwd if path is empty
@@ -222,33 +104,79 @@ func checkErr(w io.Writer, err error) (hadError bool) {
 	return false
 }
 
-// DirExists checks if a path exists and is a directory.
-// copied from afero/util, edited to return FileInfo
-func dirExists(iofs fs.FS, path string) (bool, os.FileInfo, error) {
-	fi, err := fs.Stat(iofs, path)
-	if err == nil && fi.IsDir() {
-		return true, fi, nil
-	}
-	if os.IsNotExist(err) {
-		return false, fi, nil
-	}
-	return false, fi, err
-}
-
-// fileExists checks if a path exists and is a file.
-func fileExists(iofs fs.FS, path string) (bool, error) {
-	fi, err := fs.Stat(iofs, path)
-	// TODO: maybe just check !fi.IsDir()?
-	if err == nil && fi.Mode().IsRegular() {
-		return true, nil
-	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return false, err
-}
-
 // TEMP: just for fsdata debug command
 func GetUnexportedField(field reflect.Value) interface{} {
 	return reflect.NewAt(field.Type(), unsafe.Pointer(field.UnsafeAddr())).Elem().Interface()
+}
+
+type SwitchableWriter struct {
+	mu     sync.Mutex
+	writer io.Writer
+}
+
+func (sw *SwitchableWriter) Write(p []byte) (int, error) {
+	sw.mu.Lock()
+	defer sw.mu.Unlock()
+	return sw.writer.Write(p)
+}
+
+func (sw *SwitchableWriter) Switch(w io.Writer) {
+	sw.mu.Lock()
+	sw.writer = w
+	sw.mu.Unlock()
+}
+
+// BlockingBuffer is an io.ReadWriter that blocks on read when empty.
+type BlockingBuffer struct {
+	buf    bytes.Buffer
+	mu     sync.Mutex
+	cond   *sync.Cond
+	closed bool
+}
+
+// NewBlockingBuffer creates a new BlockingBuffer.
+func NewBlockingBuffer() *BlockingBuffer {
+	bb := &BlockingBuffer{}
+	bb.cond = sync.NewCond(&bb.mu)
+	return bb
+}
+
+// Write writes data to the buffer and wakes up any blocked readers.
+func (bb *BlockingBuffer) Write(p []byte) (n int, err error) {
+	bb.mu.Lock()
+	defer bb.mu.Unlock()
+
+	if bb.closed {
+		return 0, io.ErrClosedPipe
+	}
+
+	n, err = bb.buf.Write(p)
+	bb.cond.Broadcast() // Wake up blocked readers
+	return n, err
+}
+
+// Read reads data from the buffer. It blocks if the buffer is empty.
+func (bb *BlockingBuffer) Read(p []byte) (n int, err error) {
+	bb.mu.Lock()
+	defer bb.mu.Unlock()
+
+	for bb.buf.Len() == 0 && !bb.closed {
+		bb.cond.Wait() // Wait for data to be written
+	}
+
+	if bb.closed {
+		return 0, io.EOF
+	}
+
+	return bb.buf.Read(p)
+}
+
+// Close marks the buffer as closed and wakes up any blocked readers.
+func (bb *BlockingBuffer) Close() error {
+	bb.mu.Lock()
+	defer bb.mu.Unlock()
+
+	bb.closed = true
+	bb.cond.Broadcast() // Wake up blocked readers
+	return nil
 }
