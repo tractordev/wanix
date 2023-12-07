81377ed4a872004c37ceab16d6965d7abca68db1
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2023-12-06 21:50:27 -0500

    shell: rewrite copy and fix reused flag values
    Flag values are setup as references to captured variables. These variables don't
    get reset every time a command is run, as it's assumed they'd only run once. I
    recreate the command tree after each execution to reset the flag variables. This
    isn't optimal but it's purely a design limitation.

--- shell/copy.go
+++ shell/copy.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"tractor.dev/toolkit-go/engine/cli"
 	"tractor.dev/toolkit-go/engine/fs"
@@ -12,95 +13,96 @@ import (
 	"tractor.dev/wanix/internal/osfs"
 )
 
-// TODO: merge with copyCmd
-func copyCmd2() *cli.Command {
-	cmd := &cli.Command{
-		Usage: "cp2 SOURCE DEST",
-		Args:  cli.MinArgs(2),
-		Short: "Recursively copy SOURCE to DEST. DEST must not exist.",
-		Run: func(ctx *cli.Context, args []string) {
-			srcpath := args[0]
-			dstpath := args[1]
-			err := fsutil.CopyAll(osfs.New(), absPath(srcpath), absPath(dstpath))
-			if checkErr(ctx, err) {
-				return
-			}
-		},
-	}
-	return cmd
-}
-
 func copyCmd() *cli.Command {
 	var recursive bool
+	var overwrite bool
 
 	cmd := &cli.Command{
-		Usage: "cp [-r] <SOURCE DEST | SOURCE... DIRECTORY> ",
+		Usage: "cp [-r] [-overwrite] <SOURCE DEST | SOURCE... DIRECTORY> ",
 		Args:  cli.MinArgs(2),
-		Short: "Copy SOURCE to DEST, or multiple SOURCE(s) to DIRECTORY.",
+		Short: "Copy SOURCE to DEST, or SOURCE(s) to DIRECTORY.",
+		Long: `
+Copies a single SOURCE to DEST. If DEST exists and is a directory, copy SOURCE to DEST/SOURCE_NAME.
+Can also copy multiple SOURCEs to a DIRECTORY, under DIRECTORY/SOURCE_NAME.
+Errors if final DEST already exists and "-overwrite" flag isn't specified.
+Omits any SOURCE that is a directory if "-r" flag isn't specified.
+`,
 		Run: func(ctx *cli.Context, args []string) {
-			// TODO: handle copying directories
-			isdir, err := fs.DirExists(os.DirFS("/"), unixToFsPath(args[len(args)-1]))
-			if checkErr(ctx, err) {
+			DEST := absPath(args[len(args)-1])
+			dstInfo, dstErr := os.Stat(DEST)
+			if dstErr != nil && !os.IsNotExist(dstErr) {
+				io.WriteString(ctx, fmt.Sprintln(dstErr))
 				return
 			}
-			if isdir {
-				// copy all paths to this directory
-				dir := absPath(args[len(args)-1])
 
-				for _, path := range args[:len(args)-1] {
-					srcName := filepath.Base(path)
-					dest := filepath.Join(dir, srcName)
+			DEST_exists := dstErr == nil
+			DEST_isDir := DEST_exists && dstInfo.IsDir()
 
-					srcIsDir, err := fs.IsDir(os.DirFS("/"), unixToFsPath(path))
+			fmt.Println(args)
+			if len(args)-1 >= 2 && !DEST_isDir {
+				io.WriteString(ctx, fmt.Sprintf("target '%s' is not a directory\n", args[len(args)-1]))
+				return
+			}
+
+			for _, SOURCE := range args[:len(args)-1] {
+				SOURCE = absPath(SOURCE)
+				srcInfo, err := os.Stat(SOURCE)
+				if checkErr(ctx, err) {
+					continue
+				}
+
+				if srcInfo.IsDir() && !recursive {
+					io.WriteString(ctx, fmt.Sprintf("'-r' not specified; omitting directory '%s'\n", SOURCE))
+					continue
+				}
+
+				var finalDest string
+				var finalDestExists bool
+				if DEST_isDir {
+					finalDest = filepath.Join(DEST, filepath.Base(SOURCE))
+					finalDestExists, err = fs.Exists(osfs.New(), finalDest)
 					if checkErr(ctx, err) {
 						continue
 					}
+				} else {
+					finalDest = DEST
+					finalDestExists = DEST_exists
+				}
 
-					if srcIsDir {
-						if !recursive {
-							io.WriteString(ctx, fmt.Sprintf("-r not specified; omitting directory '%s'\n", path))
-							continue
-						}
+				if srcInfo.IsDir() && strings.HasPrefix(finalDest, SOURCE) {
+					io.WriteString(ctx, fmt.Sprintf("cannot copy directory '%s' into itself, '%s'; omitting '%[0]s'\n", SOURCE, finalDest))
+					continue
+				}
 
-						err = os.MkdirAll(absPath(dest), 0755)
-						if checkErr(ctx, err) {
-							continue
-						}
+				if finalDest == SOURCE {
+					io.WriteString(ctx, fmt.Sprintf("'%[0]s' and '%[0]s' are the same file; omitting '%[0]s'\n", SOURCE))
+					continue
+				}
 
-						entries, err := os.ReadDir(absPath(path))
-						if checkErr(ctx, err) {
-							continue
-						}
+				// fmt.Printf("recursive: %v, overwrite: %v, SOURCE: %s, DEST: %s, DEST_exists: %v, DEST_isDir: %v finalDest: %s, finalDestExists: %v,\n",
+				// 	recursive, overwrite, SOURCE, DEST, DEST_exists, DEST_isDir, finalDest, finalDestExists,
+				// )
 
-						for _, e := range entries {
-							cli.Execute(ctx, copyCmd(), []string{"-r", filepath.Join(path, e.Name()), dest})
-							// commands["cp"](t, fs, []string{"-r", filepath.Join(path, e.Name()), dest})
-						}
-					} else {
-						content, err := os.ReadFile(absPath(path))
-						if checkErr(ctx, err) {
-							continue
-						}
-						err = os.WriteFile(absPath(dest), content, 0644)
-						if checkErr(ctx, err) {
-							continue
-						}
+				if finalDestExists {
+					if !overwrite {
+						io.WriteString(ctx, fmt.Sprintf("'%s' exists and '-overwrite' is not specified; omitting '%s'\n", finalDest, SOURCE))
+						continue
+					}
+
+					if err := os.RemoveAll(finalDest); checkErr(ctx, err) {
+						return
 					}
 				}
-			} else {
-				content, err := os.ReadFile(absPath(args[0]))
-				if checkErr(ctx, err) {
-					return
-				}
 
-				err = os.WriteFile(absPath(args[1]), content, 0644)
-				if checkErr(ctx, err) {
+				if err := fsutil.CopyAll(osfs.New(), SOURCE, finalDest); checkErr(ctx, err) {
 					return
 				}
 			}
 		},
 	}
 
-	cmd.Flags().BoolVar(&recursive, "r", false, "Copy recursively")
+	flags := cmd.Flags()
+	flags.BoolVar(&recursive, "r", false, "Copy file/directory(s) recursively.")
+	flags.BoolVar(&overwrite, "overwrite", false, "Overwrite any existing file/directory(s) at the destination.")
 	return cmd
 }
