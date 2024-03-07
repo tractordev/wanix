75d2bc2e107cf5920b11ab50b79294a59b87b0e5
Jeff Lindsay <progrium@gmail.com>
2024-03-06 19:27:48 -0800

    shell: add static option to avoid livereloading (for debugging)

--- shell/open.go
+++ shell/open.go
@@ -15,8 +15,10 @@ import (
 var openWatchResp js.Value = js.Null()
 
 func openCmd() *cli.Command {
+	var static bool
+
 	cmd := &cli.Command{
-		Usage: "open <appname>",
+		Usage: "open [-static] <appname>",
 		Args:  cli.ExactArgs(1),
 		Run: func(ctx *cli.Context, args []string) {
 			var path string
@@ -33,37 +35,40 @@ func openCmd() *cli.Command {
 				return
 			}
 
-			if !openWatchResp.IsNull() {
-				// close rpc channel
-				jsutil.Await(openWatchResp.Call("send", 0))
-			}
-
-			var err error
-
-			// watch(path, recursive, eventMask, ignores)
-			openWatchResp, err = jsutil.WanixSyscallResp("fs.watch", path, true, 0, []any{})
-			if err != nil {
-				fmt.Fprintln(ctx, err)
-				return
-			}
-
-			go func() {
-				for {
-					event := jsutil.Await(openWatchResp.Call("receive"))
-					if event.IsNull() {
-						return
-					}
-
-					if watchfs.EventType(event.Get("type").Int()) == watchfs.EventWrite && len(event.Get("path").String()) > len(path) {
-						// loadApp(target, path, focus)
-						jsutil.WanixSyscall("host.loadApp", "main", path, true)
-					}
+			if !static {
+				if !openWatchResp.IsNull() {
+					// close rpc channel
+					jsutil.Await(openWatchResp.Call("send", 0))
 				}
-			}()
+
+				var err error
+
+				// watch(path, recursive, eventMask, ignores)
+				openWatchResp, err = jsutil.WanixSyscallResp("fs.watch", path, true, 0, []any{})
+				if err != nil {
+					fmt.Fprintln(ctx, err)
+					return
+				}
+
+				go func() {
+					for {
+						event := jsutil.Await(openWatchResp.Call("receive"))
+						if event.IsNull() {
+							return
+						}
+
+						if watchfs.EventType(event.Get("type").Int()) == watchfs.EventWrite && len(event.Get("path").String()) > len(path) {
+							// loadApp(target, path, focus)
+							jsutil.WanixSyscall("host.loadApp", "main", path, true)
+						}
+					}
+				}()
+			}
 
 			// loadApp(target, path, focus)
 			jsutil.WanixSyscall("host.loadApp", "main", path, true)
 		},
 	}
+	cmd.Flags().BoolVar(&static, "static", false, "open without live reloading")
 	return cmd
 }
