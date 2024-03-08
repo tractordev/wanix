56989d244ebe090e8a66653cd3b17f53573611fc
Jeff Lindsay <progrium@gmail.com>
2024-03-08 13:26:47 -0800

    jazz: rework of the js behind jazz

---
+++ external/jazz/fs.ts
@@ -0,0 +1,327 @@
+import {
+  createBinaryStreamFromBlob,
+  readBlobFromBinaryStream,
+  autoSubResolution,
+} from "jazz-browser";
+import { CoMap, BinaryCoStream } from "cojson";
+
+export type FileNode = CoMap<{
+  name: string;
+  isDir: boolean;
+  size: number;
+  mtime: number;
+  fileID: BinaryCoStream["id"];
+  entries: CoMap<{[name: string]: FileNode["id"]}>;
+}>;
+
+
+// helper for jazzCall in fs/jazz.go
+export function isPromise(value) {
+  return value instanceof Promise;
+}
+
+export function cleanPath(path) {
+  return path.replace(/^.\//, '');
+}
+
+export function dirname(path) {
+  path = cleanPath(path);
+  const dir = path.replace(/\\/g,'/').replace(/\/[^\/]*$/, '');
+  if (dir === basename(path)) {
+    return "/"
+  }
+  return dir;
+}
+
+export function basename(path) {
+  path = cleanPath(path);
+  return path.replace(/\\/g,'/').split('/').pop();
+}
+
+export function unixTime() {
+  return Math.floor(Date.now() / 1000);
+}
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function ensureUint8Array(data) {
+  if (typeof data === "string") {
+    const enc = new TextEncoder("utf-8");
+    data = enc.encode(data);
+  }
+  return new Uint8Array(data);
+}
+
+
+export function blobToUint8Array(blob) {
+  return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onloadend = function() {
+          resolve(new Uint8Array(reader.result));
+      };
+
+      reader.onerror = function() {
+          reject(new Error("Failed to read blob"));
+      };
+
+      reader.readAsArrayBuffer(blob);
+  });
+}
+
+
+export function blobToString(blob) {
+  return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onloadend = function() {
+          resolve(reader.result);
+      };
+
+      reader.onerror = function() {
+          reject(new Error("Failed to read blob"));
+      };
+
+      reader.readAsText(blob);
+  });
+}
+
+
+export async function waitOn(f) {
+  let res = f();
+  let count = 0;
+  while (!res) {
+    await delay(50);
+    res = f();
+    count++;
+    if (count > 25) {
+      throw new Error("value not resolving")
+    }
+  }
+  return res;
+}
+
+// file data
+
+export async function fetchFile(id) {
+  const blob = await readBlobFromBinaryStream(id, window.node);
+  return await blobToUint8Array(blob);
+}
+
+export async function makeFile(data, group) {
+  if (!group) {
+    group = window.space.meta.group;
+  }
+  const blob = new Blob([data], {type: "text/plain"});
+  const bs = await createBinaryStreamFromBlob(blob, group);
+  return bs;
+}
+
+// file node
+
+export async function makeDirNode(name, group) {
+  if (!group) {
+    group = window.space.meta.group;
+  }
+  const entries = group.createMap();
+  const dir = group.createMap<FileNode>({
+    name: name,
+    isDir: true,
+    size: 0,
+    mtime: unixTime(),
+    entries: entries
+  });
+  return await autoSubResolution(dir.id, (d) => d, window.node);
+}
+
+export async function makeFileNode(name, data, group) {
+  if (!group) {
+    group = window.space.meta.group;
+  }
+  data = ensureUint8Array(data);
+  const file = await makeFile(data, group);
+  return group.createMap<FileNode>({
+    name: name,
+    isDir: false,
+    size: data.byteLength,
+    mtime: unixTime(),
+    fileID: file.id
+  });
+}
+
+export async function nodeReload(n) {
+  return await autoSubResolution(n.id, (n) => n, window.node);
+}
+
+export async function nodeDir(n) {
+  const entries = await waitOn(() => n.entries);
+  return Object.keys(entries);
+}
+
+export async function nodeFileGet(n) {
+  if (n.meta) {
+    return await fetchFile(n.meta.coValue.get("fileID"));
+  } else {
+    return await fetchFile(n.get("fileID"));
+  }
+}
+
+export async function nodeFilePut(n, data) {
+  data = ensureUint8Array(data);
+  const file = await makeFile(data);
+  n.mutate(n => {
+    n.set("size", data.byteLength);
+    n.set("mtime", unixTime());
+    n.set("fileID", file.id);
+  });
+}
+
+export async function nodeAdd(n, node) {
+  const entries = await waitOn(() => n.entries);
+  entries.mutate(n => {
+    n.set(node.get("name"), node.id);
+  });
+  await nodeTouch(n);
+}
+
+export async function nodeTouch(n) {
+  n.mutate(n => {
+    n.set("mtime", unixTime());
+  });
+}
+
+export async function nodeRemove(n, name) {
+  const entries = await waitOn(() => n.entries);
+  entries.mutate(n => {
+    n.delete(name);
+  });
+  await nodeTouch(n);
+}
+
+export async function walk(path: string): any {
+  path = cleanPath(path);
+  let cur = await window.jazz.root();
+  if (path === "." || path === "" || path === "/") {
+    return cur;
+  }
+  let parts = path.replace(/^\//, '').split('/');
+  for (const name of parts) {
+    if (!name) {
+      continue;
+    }
+    if (!cur.isDir) {
+      break;
+    }
+    let names = await nodeDir(cur);
+    if (!names.includes(name)) {
+      return null;
+    }
+    cur = await waitOn(() => cur.entries[name]);
+    if (cur === undefined) {
+      return undefined;
+    }
+  }
+  return cur;
+}
+
+// path operations
+
+export async function stat(path) {
+  const node = await walk(path);
+  if (!node) {
+    return null;
+  }
+  return node.meta.coValue.toJSON();
+}
+
+export async function readdir(path) {
+  return await nodeDir(await walk(path));
+}
+
+export async function mkdir(path) {
+  const node = await walk(dirname(path));
+  if (!node) {
+    console.log("mkdir: no parent dir", dirname(path));
+    return;
+  }
+  const dir = await makeDirNode(basename(path));
+  await nodeAdd(node, dir);
+}
+
+export async function mkdirAll(path) {
+  const parts = cleanPath(path).split("/");
+  let fullPath = "";
+  const fullPaths = [];
+  for (const part of parts) {
+    fullPath += `/${part}`;
+    fullPaths.push(fullPath);
+  }
+  let curParent = await window.jazz.root();;
+  for (const dirpath of fullPaths) {
+    const n = await walk(dirpath.slice(1));
+    if (n) {
+      curParent = n;
+      continue;
+    }
+    const dir = await makeDirNode(basename(dirpath));
+    await nodeAdd(curParent, dir);
+    curParent = dir;
+  }
+}
+
+export async function remove(path) {
+  const n = await walk(dirname(path));
+  if (!n) {
+    return null;
+  }
+  await nodeRemove(n, basename(path));
+}
+
+export async function rename(oldpath, newpath) {
+  const node = await walk(oldpath);
+  if (!node) {
+    return null;
+  }
+  if (basename(oldpath) !== basename(newpath)) {
+    node.mutate(n => {
+      n.set("name", basename(newpath));
+    });
+  }
+  const olddir = await walk(dirname(oldpath));
+  if (!olddir) {
+    return null;
+  }
+  const newdir = await walk(dirname(newpath));
+  if (!newdir) {
+    return null;
+  }
+  await nodeAdd(newdir, node);
+  await nodeRemove(olddir, basename(oldpath));
+}
+
+export async function readFile(path) {
+  const node = await walk(path);
+  if (!node) {
+    return null;
+  }
+  return await nodeFileGet(node);
+}
+
+
+export async function writeFile(path, content) {
+  const node = await walk(path);
+  if (!node) {
+    const dir = await walk(dirname(path));
+    if (!dir) {
+      return null;
+    }
+    const f = await makeFileNode(basename(path), content);
+    await nodeAdd(dir, f);
+    return true;
+  }
+  await nodeFilePut(node, content);
+  return true;
+}
+
---
+++ external/jazz/jazz.ts
@@ -0,0 +1,168 @@
+import { BrowserAuth0 } from "jazz-browser-auth0";
+import {
+  autoSub,
+  autoSubResolution,
+  createBrowserNode,
+  parseInviteLink,
+} from "jazz-browser";
+import {cojsonInternals} from "cojson";
+import * as fs from "./fs.ts";
+
+export {
+  autoSub,
+  autoSubResolution,
+};
+
+
+
+export function createInviteLinkHash(value, role) {
+  const coValueCore =
+        "coValueType" in value ? value.meta.coValue.core : value.core;
+  let currentCoValue = coValueCore;
+
+  while (currentCoValue.header.ruleset.type === "ownedByGroup") {
+      currentCoValue = currentCoValue.getGroup().core;
+  }
+
+  if (currentCoValue.header.ruleset.type !== "group") {
+      throw new Error("Can't create invite link for object without group");
+  }
+
+  const group = cojsonInternals.expectGroup(
+    currentCoValue.getCurrentContent()
+  )
+  return `#/invite/${value.id}/${group.createInvite(role)}`;
+}
+
+export async function setupFrameSpace(node, migration) {
+  const listener = async () => {
+    const invite = parseInviteLink(parent.location.href);
+    if (invite) {
+      try {
+        await node.acceptInvite(invite.valueID, invite.inviteSecret);
+      } catch {
+        console.warn("invite not accepted");
+        return;
+      }
+      parent.location.hash = invite.valueID;
+    }
+    
+    let spaceID = parent.location.hash.slice(1) || undefined;
+    if (!spaceID) {
+      const group = node.createGroup();
+      const space = group.createMap();
+      if (migration) {
+        await migration(space, group);
+      }
+      parent.location.hash = space.id;
+    }
+  };
+  
+  parent.addEventListener("hashchange", listener);
+  await listener();
+
+  return await autoSubResolution(parent.location.hash.slice(1), (s) => s, node);
+}
+
+export async function setupWorkerSpace(node, migration) {
+  const hostHash = async (value) => {
+    if (value) {
+      await globalThis.sys.call("host.setHash", [value]);
+      return;
+    }
+    return (await globalThis.sys.call("host.hash", [])).value;
+  }
+  const url = location.origin+"/#"+(await hostHash());
+
+  const invite = parseInviteLink(url);
+  if (invite) {
+    try {
+      await node.acceptInvite(invite.valueID, invite.inviteSecret);
+    } catch {
+      console.warn("invite not accepted");
+      return;
+    }
+    await hostHash(invite.valueID);
+  }
+
+  const spaceID = await hostHash() || undefined;
+  if (!spaceID) {
+    const group = node.createGroup();
+    const space = group.createMap();
+    if (migration) {
+      await migration(space, group);
+    }
+    await hostHash(space.id);
+  }
+
+  return await autoSubResolution(await hostHash(), (s) => s, node);
+}
+
+export async function initNode(name, domain, clientID, accessToken, migration) {
+  const { node, done } = await createBrowserNode({
+    auth: new BrowserAuth0({domain, clientID, accessToken}, domain, name),
+    migration
+  });
+  return node;
+}
+
+export async function initJazz(globalObj) {
+  let setupFn = setupFrameSpace;
+  let getItem = async (key) => localStorage.getItem(key);
+  if (globalObj.hostURL) {
+    // in worker
+    setupFn = setupWorkerSpace;
+    getItem = async (key) => {return (await globalObj.sys.call("host.getItem", [key])).value};
+  }
+
+  const jazzEnabled = await getItem("jazz:enabled");
+  if (!jazzEnabled) {
+    console.warn("jazz not enabled");
+    return null;
+  }
+  const settingsData = await getItem("auth:settings");
+  if (!settingsData) {
+    console.warn("auth settings not found")
+    return null;
+  }
+  const loginData = await getItem("auth:login");
+  if (!loginData) {
+    console.warn("auth login not found")
+    return null;
+  }
+  const settings = JSON.parse(settingsData);
+  const login = JSON.parse(loginData);
+
+  const node = await initNode(
+    "wanix:"+location.origin.split("//")[1], 
+    settings["domain"],
+    settings["clientId"],
+    login["access_token"],
+    (account) => { },
+  );
+
+  // hack for fs api
+  globalThis.node = node;
+  
+  const space = await setupFn(node, async (space, group) => {
+    const root = await fs.makeDirNode("/", group);
+    space.mutate(s => {
+      s.set("fs", root.id);
+    });
+  });
+
+  // hack for fs api
+  globalThis.space = space;
+
+  const username = await autoSubResolution("me", (me) => me.profile?.name, node);
+  const inviteURL = location.origin+"/"+createInviteLinkHash(space, "admin");
+
+  globalObj.jazz = {
+    node,
+    space,
+    username,
+    inviteURL,
+    fsutil: fs,
+    root: async () => await autoSubResolution(space.id, (s) => s?.fs, node)
+  };
+}
