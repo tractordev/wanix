f20b7330800e4d707d2307c20acc4eb1f3d41740
Jeff Lindsay <progrium@gmail.com>
2025-02-17 20:39:07 -0800

    web: api updates and add service worker

--- web/web.go
+++ web/web.go
@@ -3,25 +3,33 @@
 package web
 
 import (
+	"syscall/js"
+
 	"tractor.dev/wanix/fs"
 	"tractor.dev/wanix/fs/fskit"
-	"tractor.dev/wanix/kernel/fsys"
+	"tractor.dev/wanix/kernel"
 	"tractor.dev/wanix/web/dom"
 	"tractor.dev/wanix/web/fsa"
+	"tractor.dev/wanix/web/sw"
 	"tractor.dev/wanix/web/vm"
 	"tractor.dev/wanix/web/worker"
 )
 
-func New(fsys *fsys.Device) fskit.MapFS {
-	fsys.Register("pickerfs", func(s []string) (fs.FS, error) {
+func New(k *kernel.K, ctx js.Value) fskit.MapFS {
+	k.Fsys.Register("pickerfs", func(s []string) (fs.FS, error) {
 		return fsa.ShowDirectoryPicker(), nil
 	})
-	fsys.Register("opfs", func(s []string) (fs.FS, error) {
+	k.Fsys.Register("opfs", func(s []string) (fs.FS, error) {
 		return fsa.OPFS()
 	})
-	return fskit.MapFS{
+	webfs := fskit.MapFS{
 		"dom":    dom.New(),
 		"vm":     vm.New(),
-		"worker": worker.New(),
+		"worker": worker.New(k),
 	}
+	if !ctx.Get("sw").IsUndefined() {
+		webfs["sw"] = sw.Activate(ctx.Get("sw"), k)
+	}
+
+	return webfs
 }
