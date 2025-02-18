61b4f80ebaefec01cb031933ccc746860cd5346a
Jeff Lindsay <progrium@gmail.com>
2025-02-17 20:36:39 -0800

    web: add service worker module

---
+++ wasm/assets/sw.js
@@ -0,0 +1,79 @@
+
+self.addEventListener("activate", event => {
+    event.waitUntil(clients.claim());
+});
+
+let listener = undefined;
+self.addEventListener("message", (event) => {
+    if (event.data.listen) {
+        listener = event.data.listen;
+        console.log("SW: listener installed");
+        return;
+    }
+});
+
+self.addEventListener("fetch", async (event) => {
+    if (!listener) return;
+
+    const req = event.request;
+    const url = new URL(req.url);
+    const headers = {}
+    for (var p of req.headers) {
+        headers[p[0]] = p[1]
+    }
+
+    event.respondWith(new Promise(async (resolve) => {
+        const ch = new MessageChannel();
+        const response = new Promise(respond => {
+            ch.port1.onmessage = (event) => respond(event.data);
+        });
+        listener.postMessage({
+            request: {
+                method: req.method, 
+                url: req.url, 
+                headers: headers,
+                host: url.host,
+                hostname: url.hostname,
+                pathname: url.pathname,
+                port: url.port,
+            },
+            responder: ch.port2
+        }, [ch.port2]);
+        try {
+            const reply = await Promise.race([response, timeout(1000)]);
+
+            if (reply.error) {
+                throw new Error(reply.error);
+            }
+
+            if (reply.status === 100) {
+                resolve(fetchBypass(req));
+                return;
+            }
+
+            resolve(new Response(reply.body, reply));
+            
+        } catch (error) {
+            console.warn("SW:", error);
+            if (error.message === 'Timeout') {
+                listener = undefined;
+            }
+            resolve(fetchBypass(req));
+        }
+    }));
+});
+
+
+function timeout(ms) {
+    return new Promise((_, reject) => setTimeout(() => reject(new Error('Timeout')), ms));
+}
+
+function fetchBypass(request) {
+    // Create a new request with mode 'no-cors' to bypass the service worker
+    const newRequest = new Request(request.url, {
+        method: request.method,
+        headers: request.headers,
+        mode: 'no-cors',
+    });
+    return fetch(newRequest);
+}
---
+++ web/sw/sw.go
@@ -0,0 +1,134 @@
+//go:build js && wasm
+
+package sw
+
+import (
+	"bytes"
+	"context"
+	"net/http"
+	"strings"
+	"syscall/js"
+
+	"tractor.dev/toolkit-go/engine/cli"
+	"tractor.dev/wanix/fs"
+	"tractor.dev/wanix/fs/fskit"
+	"tractor.dev/wanix/internal/httprecorder"
+	"tractor.dev/wanix/kernel"
+	"tractor.dev/wanix/misc"
+	"tractor.dev/wanix/web/jsutil"
+)
+
+type Device struct {
+	active js.Value
+	k      *kernel.K
+}
+
+func Activate(ch js.Value, k *kernel.K) *Device {
+	reg := jsutil.Await(jsutil.Get("navigator.serviceWorker").Call("getRegistration"))
+	if reg.IsUndefined() {
+		swPath := "./sw.js"
+		jsutil.Await(jsutil.Get("navigator.serviceWorker").Call("register", swPath, map[string]any{"type": "module"}))
+		reg = jsutil.Await(jsutil.Get("navigator.serviceWorker.ready"))
+	}
+
+	d := &Device{
+		active: reg.Get("active"),
+		k:      k,
+	}
+	ch.Get("port2").Set("onmessage", js.FuncOf(d.handleMessage))
+
+	reg.Get("active").Call("postMessage", map[string]any{"listen": ch.Get("port1")}, []any{ch.Get("port1")})
+	return d
+}
+
+func (d *Device) Sub(name string) (fs.FS, error) {
+	fsys := fskit.MapFS{
+		"ctl": misc.ControlFile(&cli.Command{
+			Usage: "ctl",
+			Short: "control the active service worker",
+			Run: func(ctx *cli.Context, args []string) {
+				switch args[0] {
+				case "start":
+
+					// d.state = "running"
+				case "terminate":
+					// if !r.worker.IsUndefined() {
+					// 	r.worker.Call("terminate")
+					// }
+					// d.state = "terminated"
+				}
+			},
+		}),
+		"state": misc.FieldFile(d.active.Get("state").String()),
+		// "err": misc.FieldFile(r.state, nil),
+		// "fsys": misc.FieldFile(r.fs, nil),
+	}
+	return fs.Sub(fsys, name)
+}
+
+func (d *Device) Open(name string) (fs.File, error) {
+	return d.OpenContext(context.Background(), name)
+}
+
+func (d *Device) OpenContext(ctx context.Context, name string) (fs.File, error) {
+	fsys, err := d.Sub(".")
+	if err != nil {
+		return nil, err
+	}
+	return fs.OpenContext(ctx, fsys, name)
+}
+
+func (d *Device) handleMessage(this js.Value, args []js.Value) interface{} {
+	if args[0].Get("data").Get("request").IsUndefined() {
+		return nil
+	}
+
+	// Create a goroutine to handle the request asynchronously
+	go func() {
+		jsReq := args[0].Get("data").Get("request")
+		jsResp := args[0].Get("data").Get("responder")
+
+		req, err := http.NewRequest(jsReq.Get("method").String(), jsReq.Get("url").String(), nil)
+		if err != nil {
+			jsResp.Call("postMessage", js.ValueOf(map[string]interface{}{
+				"status":     500,
+				"statusText": "Gateway error",
+				"body":       err.Error(),
+			}))
+			return
+		}
+		rw := httprecorder.NewRecorder()
+
+		d.ServeHTTP(rw, req)
+
+		headers := make(map[string]any)
+		for k, v := range rw.Result().Header {
+			headers[k] = v[0]
+		}
+		var buf bytes.Buffer
+		rw.Body.WriteTo(&buf)
+
+		jsResp.Call("postMessage", js.ValueOf(map[string]interface{}{
+			"status":     rw.Code,
+			"statusText": rw.Result().Status,
+			"body":       buf.String(),
+		}))
+	}()
+
+	return nil
+}
+
+func (d *Device) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if strings.HasPrefix(r.URL.Path, "/sw/") {
+		path := strings.TrimPrefix(r.URL.Path, "/sw/")
+		b, err := fs.ReadFile(d.k.NS, path)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(b)
+		return
+	}
+	w.WriteHeader(http.StatusContinue)
+}
