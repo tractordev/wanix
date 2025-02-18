805559cdf6b525ada6d8c70f307f77522a302e5a
Jeff Lindsay <progrium@gmail.com>
2025-02-18 00:44:28 -0800

    wasm: put virtio9p code in its own package. call bootVM() by default unless ?autoboot=false

---
+++ internal/virtio9p/virtio9p.go
@@ -0,0 +1,76 @@
+//go:build js && wasm
+
+package virtio9p
+
+import (
+	"io"
+	"io/fs"
+	"log"
+	"syscall/js"
+
+	"github.com/hugelgupf/p9/p9"
+	"tractor.dev/wanix/fs/p9kit"
+)
+
+func StartFor(fsys fs.FS, ctx js.Value, debug bool) {
+	inR, inW := io.Pipe()
+	outR, outW := io.Pipe()
+
+	var virtioSend js.Value
+
+	ctx.Set("virtioHandle", js.FuncOf(func(this js.Value, args []js.Value) any {
+		virtioSend = args[1]
+		go func() {
+			buf := make([]byte, args[0].Get("byteLength").Int())
+			js.CopyBytesToGo(buf, args[0])
+			_, err := inW.Write(buf)
+			if err != nil {
+				log.Println(err)
+			}
+			if debug {
+				log.Printf("virtio >>%s %v\n", p9kit.MessageTypes[int(buf[4])], buf)
+			}
+		}()
+		return nil
+	}))
+
+	go func() {
+		for {
+			// Read message length (4 bytes)
+			sizeBuf := make([]byte, 4)
+			_, err := io.ReadFull(outR, sizeBuf)
+			if err != nil {
+				log.Println("9p->virtio:", err)
+				break
+			}
+			messageSize := int(sizeBuf[3])<<24 | int(sizeBuf[2])<<16 | int(sizeBuf[1])<<8 | int(sizeBuf[0])
+			payloadSize := messageSize - 4
+
+			messageBuf := make([]byte, payloadSize)
+			_, err = io.ReadFull(outR, messageBuf)
+			if err != nil {
+				log.Println("9p-virtio:", err)
+				break
+			}
+
+			buf := append(sizeBuf, messageBuf...)
+
+			jsBuf := js.Global().Get("Uint8Array").New(len(buf))
+			js.CopyBytesToJS(jsBuf, buf)
+			if virtioSend.IsUndefined() {
+				log.Println("virtioSend is undefined")
+				return
+			}
+			if debug {
+				log.Printf("virtio <<%s %v\n", p9kit.MessageTypes[int(buf[4])], buf)
+			}
+			virtioSend.Invoke(jsBuf)
+		}
+	}()
+	srv := p9.NewServer(p9kit.Attacher(fsys))
+	go func() {
+		if err := srv.Handle(inR, outW); err != nil {
+			log.Fatal(err)
+		}
+	}()
+}
--- wasm/main.go
+++ wasm/main.go
@@ -3,13 +3,11 @@
 package main
 
 import (
-	"io"
 	"log"
 	"syscall/js"
 
-	"github.com/hugelgupf/p9/p9"
 	"tractor.dev/wanix/fs"
-	"tractor.dev/wanix/fs/p9kit"
+	"tractor.dev/wanix/internal/virtio9p"
 	"tractor.dev/wanix/kernel"
 	"tractor.dev/wanix/web"
 	"tractor.dev/wanix/web/api"
@@ -29,65 +27,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	inR, inW := io.Pipe()
-	outR, outW := io.Pipe()
-	var virtioSend js.Value
-	ctx.Set("virtioHandle", js.FuncOf(func(this js.Value, args []js.Value) any {
-		virtioSend = args[1]
-		go func() {
-			buf := make([]byte, args[0].Get("byteLength").Int())
-			js.CopyBytesToGo(buf, args[0])
-			_, err := inW.Write(buf)
-			if err != nil {
-				log.Println(err)
-			}
-			log.Printf("virtio >>%s %v\n", p9kit.MessageTypes[int(buf[4])], buf)
-		}()
-		return nil
-	}))
-
-	go func() {
-		for {
-			// Read message length (4 bytes)
-			sizeBuf := make([]byte, 4)
-			_, err := io.ReadFull(outR, sizeBuf)
-			if err != nil {
-				log.Println("outR read size error:", err)
-				break
-			}
-			messageSize := int(sizeBuf[3])<<24 | int(sizeBuf[2])<<16 | int(sizeBuf[1])<<8 | int(sizeBuf[0])
-
-			// Subtract 4 bytes from messageSize since we've already read the size field
-			payloadSize := messageSize - 4
-
-			// Read the remaining message payload
-			messageBuf := make([]byte, payloadSize)
-			_, err = io.ReadFull(outR, messageBuf)
-			if err != nil {
-				log.Println("outR read message error:", err)
-				break
-			}
-
-			// Combine size field and payload for complete message
-			buf := append(sizeBuf, messageBuf...)
-
-			jsBuf := js.Global().Get("Uint8Array").New(len(buf))
-			js.CopyBytesToJS(jsBuf, buf)
-			if virtioSend.IsUndefined() {
-				log.Println("virtioSend is undefined")
-				return
-			}
-			log.Printf("virtio <<%s %v\n", p9kit.MessageTypes[int(buf[4])], buf)
-			virtioSend.Invoke(jsBuf)
-		}
-	}()
-	srv := p9.NewServer(p9kit.Attacher(root.Namespace()))
-	go func() {
-		if err := srv.Handle(inR, outW); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
 	root.Bind("#fsys", "fsys")
 	root.Bind("#proc", "proc")
 	root.Bind("#web", "web")
@@ -96,5 +35,6 @@ func main() {
 	fs.WriteFile(root.Namespace(), "fsys/1/ctl", []byte("mount"), 0755)
 	fs.WriteFile(root.Namespace(), "proc/1/ctl", []byte("bind fsys/1/mount opfs"), 0755)
 
+	virtio9p.StartFor(root.Namespace(), ctx, false)
 	api.PortResponder(ctx.Get("sys"), root)
 }
