d5d0d8d47656c37203903da63c00bc7cc88d3878
Jeff Lindsay <progrium@gmail.com>
2025-03-04 18:44:33 -0800

    linux: add env and stderr to wexec. also wait until stdout is finished before quiting

--- external/linux/wexec/main.go
+++ external/linux/wexec/main.go
@@ -7,12 +7,18 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
+func debug(format string, a ...any) {
+	// log.Printf(format+"\n", a...)
+}
+
 func main() {
 	if len(os.Args) < 2 {
-		log.Fatal("usage: wexec <command> [args...]")
+		log.Fatal("usage: wexec <wasm> [args...]")
 	}
 
 	wd, err := os.Getwd()
@@ -22,31 +28,65 @@ func main() {
 	args := os.Args[1:]
 	args[0] = strings.TrimPrefix(filepath.Join(wd, args[0]), "/wanix/")
 
+	debug("allocating pid")
 	pidRaw, err := os.ReadFile("/wanix/proc/new/wasi")
 	if err != nil {
 		log.Fatal(err)
 	}
 	pid := strings.TrimSpace(string(pidRaw))
 
+	debug("writing cmd")
 	if err := appendFile(fmt.Sprintf("/wanix/proc/%s/cmd", pid), []byte(strings.Join(args, " "))); err != nil {
 		log.Fatal(err)
 	}
 
+	debug("writing env")
+	env := strings.Join(append(os.Environ(), ""), "\n")
+	if err := appendFile(fmt.Sprintf("/wanix/proc/%s/env", pid), []byte(env)); err != nil {
+		log.Fatal(err)
+	}
+
+	var done atomic.Int32
+	var wg sync.WaitGroup
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
+		debug("polling fd/1 => stdout")
 		for {
 			b, err := os.ReadFile(fmt.Sprintf("/wanix/proc/%s/fd/1", pid))
 			if err != nil {
 				log.Fatal(err)
 			}
+			if done.Load() == 1 && len(b) == 0 {
+				return
+			}
 			os.Stdout.Write(b)
 			time.Sleep(200 * time.Millisecond)
 		}
 	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		debug("polling fd/2 => stderr")
+		for {
+			b, err := os.ReadFile(fmt.Sprintf("/wanix/proc/%s/fd/2", pid))
+			if err != nil {
+				log.Fatal(err)
+			}
+			if done.Load() == 1 && len(b) == 0 {
+				return
+			}
+			os.Stderr.Write(b)
+			time.Sleep(200 * time.Millisecond)
+		}
+	}()
 
+	debug("starting")
 	if err := appendFile(fmt.Sprintf("/wanix/proc/%s/ctl", pid), []byte("start")); err != nil {
 		log.Fatal(err)
 	}
 
+	debug("waiting for exit")
 	for {
 		b, err := os.ReadFile(fmt.Sprintf("/wanix/proc/%s/exit", pid))
 		if err != nil {
@@ -58,6 +98,8 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
+			done.Store(1)
+			wg.Wait()
 			os.Exit(code)
 		}
 		time.Sleep(200 * time.Millisecond)
