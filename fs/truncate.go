e11d1d0708e9cdb167579c8b1bec6f39f2ca01ea
Jeff Lindsay <progrium@gmail.com>
2025-03-04 18:54:18 -0800

    fs: use Sub in Truncate

--- fs/truncate.go
+++ fs/truncate.go
@@ -1,5 +1,11 @@
 package fs
 
+import (
+	"fmt"
+	"path"
+	"reflect"
+)
+
 type TruncateFS interface {
 	FS
 	Truncate(name string, size int64) error
@@ -8,6 +14,26 @@ type TruncateFS interface {
 func Truncate(fsys FS, name string, size int64) error {
 	if t, ok := fsys.(TruncateFS); ok {
 		return t.Truncate(name, size)
+	}
+
+	if path.Dir(name) != "." {
+		parent, err := Sub(fsys, path.Dir(name))
+		if err != nil {
+			return err
+		}
+		if subfs, ok := parent.(*SubdirFS); ok && reflect.DeepEqual(subfs.Fsys, fsys) {
+			// if parent is a SubdirFS of our fsys, we manually
+			// call Truncate to avoid infinite recursion
+			full, err := subfs.fullName("truncate", path.Base(name))
+			if err != nil {
+				return err
+			}
+			if m, ok := subfs.Fsys.(TruncateFS); ok {
+				return m.Truncate(full, size)
+			}
+			return fmt.Errorf("%w on %T: Truncate %s", ErrNotSupported, subfs.Fsys, full)
+		}
+		return Truncate(parent, path.Base(name), size)
 	}
 
 	b, err := ReadFile(fsys, name)
