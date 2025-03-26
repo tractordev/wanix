d29da7692fa25f0c46a9d2d399c3940062f71119
Jeff Lindsay <progrium@gmail.com>
2025-03-26 16:04:41 -0700

    sw: add initial caching for offline mode

--- wasm/assets/sw.js
+++ wasm/assets/sw.js
@@ -1,6 +1,30 @@
 
+let cache = undefined;
 self.addEventListener("activate", event => {
     event.waitUntil(clients.claim());
+    console.log("ServiceWorker: activated:", self.location.href);
+    caches.open("v0").then(c => {
+        cache = c;
+        cache.addAll([
+            "/",
+            "/index.html",
+
+            // goal:
+            // "/wanix-0.3.min.js",
+            // "/wanix-0.3.sw.js",
+            // "/wanix-0.3.wasm",
+
+            "/wanix.css",
+            "/wanix.js",
+            "/wanix.wasm",
+            
+            "/v86/libv86.js",
+            "/wasm_exec.js",
+            "/wio.js",
+            
+            "/duplex.min.js",
+        ]);
+    });
 });
 
 let listener = undefined;
@@ -13,56 +37,28 @@ self.addEventListener("message", (event) => {
 });
 
 self.addEventListener("fetch", async (event) => {
-    if (!listener) return;
+    if (!listener) {
+        console.warn("ServiceWorker: no backend registered");
+        return;
+    }
 
     const req = event.request;
-    const url = new URL(req.url);
-    const headers = {};
-    for (var p of req.headers) {
-        headers[p[0]] = p[1];
-    }
-    headers["X-ServiceWorker"] = self.location.href;
-
-    event.respondWith(new Promise(async (resolve) => {
-        const ch = new MessageChannel();
-        const response = new Promise(respond => {
-            ch.port1.onmessage = (event) => respond(event.data);
-        });
-        listener.postMessage({
-            request: {
-                method: req.method, 
-                url: req.url, 
-                headers: headers,
-                host: url.host,
-                hostname: url.hostname,
-                pathname: url.pathname,
-                port: url.port,
-            },
-            responder: ch.port2
-        }, [ch.port2]);
-        try {
-            const reply = await Promise.race([response, timeout(1000)]);
-
-            if (reply.error) {
-                throw new Error(reply.error);
-            }
-
-            if (reply.status === 100) {
-                resolve(fetchBypass(req));
-                return;
-            }
-
-            resolve(new Response(reply.body, reply));
-            
-        } catch (error) {
-            if (error.message === 'Timeout') {
-                listener = undefined;
-            } else {
-                console.warn("ServiceWorker:", error);
-            }
-            resolve(fetchBypass(req));
+    
+    event.respondWith(cache.match(req).then(async (resp) => {
+        if (resp) {
+            console.log("ServiceWorker: cache hit", req.url);
+            return resp;
         }
+
+        resp = await backendReponse(req);
+        if (resp) {
+            cache.put(req, resp.clone());
+            console.log("ServiceWorker: cached", req.url);
+        }
+
+        return resp;
     }));
+
 });
 
 
@@ -78,4 +74,51 @@ function fetchBypass(request) {
         mode: 'no-cors',
     });
     return fetch(newRequest);
+}
+
+async function backendReponse(req) {
+    const url = new URL(req.url);
+    const headers = {};
+    for (var p of req.headers) {
+        headers[p[0]] = p[1];
+    }
+    headers["X-Service-Worker"] = self.location.href;
+
+    const ch = new MessageChannel();
+    const response = new Promise(respond => {
+        ch.port1.onmessage = (event) => respond(event.data);
+    });
+    listener.postMessage({
+        request: {
+            method: req.method, 
+            url: req.url, 
+            headers: headers,
+            host: url.host,
+            hostname: url.hostname,
+            pathname: url.pathname,
+            port: url.port,
+        },
+        responder: ch.port2
+    }, [ch.port2]);
+    try {
+        const reply = await Promise.race([response, timeout(1000)]);
+
+        if (reply.error) {
+            throw new Error(reply.error);
+        }
+
+        if (reply.status === 100) {
+            return fetchBypass(req);
+        }
+
+        return new Response(reply.body, reply);
+        
+    } catch (error) {
+        if (error.message === 'Timeout') {
+            listener = undefined;
+        } else {
+            console.warn("ServiceWorker:", error);
+        }
+        return fetchBypass(req);
+    }
 }
