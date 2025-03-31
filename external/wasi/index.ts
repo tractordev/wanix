cfd0a86ad5298489d408a9bb9c3c4e797a40558e
Jeff Lindsay <progrium@gmail.com>
2025-03-31 14:36:03 -0700

    wasi: build self contained bundle with WanixFS and duplex included

--- external/wasi/index.ts
+++ external/wasi/index.ts
@@ -4,3 +4,5 @@ export { OpenFile, File, Directory, PreopenDirectory } from "./fs.ts";
 export { EmptyFile, OpenEmptyFile } from "./misc.ts";
 export { WASI, ConsoleStdout } from "@bjorn3/browser_wasi_shim";
 export { applyPatchPollOneoff } from "./poll-oneoff.ts";
+
+export { WanixFS } from "./wanixjs/wanix.js";
