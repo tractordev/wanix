e365b0d767f291072554e0bff47a0daee1df0099
Jeff Lindsay <progrium@gmail.com>
2025-04-07 22:07:19 -0700

    worker: moving around definitions for demo

--- web/worker/service.go
+++ web/worker/service.go
@@ -25,6 +25,42 @@ func New(task *task.Resource) *Service {
 	}
 }
 
+func (d *Service) Open(name string) (fs.File, error) {
+	return d.OpenContext(context.Background(), name)
+}
+
+func (d *Service) OpenContext(ctx context.Context, name string) (fs.File, error) {
+	fsys, rname, err := d.ResolveFS(ctx, name)
+	if err != nil {
+		return nil, err
+	}
+	return fs.OpenContext(ctx, fsys, rname)
+}
+
+func (d *Service) ResolveFS(ctx context.Context, name string) (fs.FS, string, error) {
+	return fs.Resolve(fskit.UnionFS{
+		fskit.MapFS{
+			"new": fskit.OpenFunc(func(ctx context.Context, name string) (fs.File, error) {
+				if name == "." {
+					return &fskit.FuncFile{
+						Node: fskit.Entry(name, 0555),
+						ReadFunc: func(n *fskit.Node) error {
+							r, err := d.Alloc(d.task)
+							if err != nil {
+								return err
+							}
+							fskit.SetData(n, []byte(r.ID()+"\n"))
+							return nil
+						},
+					}, nil
+				}
+				return nil, fs.ErrNotExist
+			}),
+		},
+		fskit.MapFS(d.resources),
+	}, ctx, name)
+}
+
 func (d *Service) Alloc(t *task.Resource) (*Resource, error) {
 	if t == nil {
 		t = d.task
@@ -39,37 +75,4 @@ func (d *Service) Alloc(t *task.Resource) (*Resource, error) {
 	}
 	d.resources[rid] = r
 	return r, nil
-}
-
-func (d *Service) ResolveFS(ctx context.Context, name string) (fs.FS, string, error) {
-	return fs.Resolve(fskit.UnionFS{fskit.MapFS{
-		"new": fskit.OpenFunc(func(ctx context.Context, name string) (fs.File, error) {
-			if name == "." {
-				return &fskit.FuncFile{
-					Node: fskit.Entry(name, 0555),
-					ReadFunc: func(n *fskit.Node) error {
-						r, err := d.Alloc(d.task)
-						if err != nil {
-							return err
-						}
-						fskit.SetData(n, []byte(r.ID()+"\n"))
-						return nil
-					},
-				}, nil
-			}
-			return nil, fs.ErrNotExist
-		}),
-	}, fskit.MapFS(d.resources)}, ctx, name)
-}
-
-func (d *Service) Open(name string) (fs.File, error) {
-	return d.OpenContext(context.Background(), name)
-}
-
-func (d *Service) OpenContext(ctx context.Context, name string) (fs.File, error) {
-	fsys, rname, err := d.ResolveFS(ctx, name)
-	if err != nil {
-		return nil, err
-	}
-	return fs.OpenContext(ctx, fsys, rname)
 }
