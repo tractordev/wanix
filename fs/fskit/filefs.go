023dcf77fc1d97bcaf94a4775960b8ca9f47096b
Jeff Lindsay <progrium@gmail.com>
2025-08-08 00:57:27 -0700

    fskit: cleanup unnecessary code from gpt5

--- fs/fskit/filefs.go
+++ fs/fskit/filefs.go
@@ -8,40 +8,25 @@ import (
 )
 
 // FileFS wraps a single fs.File as an fs.FS such that opening
-// "." returns that file (not a directory). This matches places in
-// the project that expect the FS root to be the file itself.
+// "." returns that file (not a directory).
 func FileFS(file fs.File, name string) fs.FS {
-	return &fileFS{file: file, name: name}
+	if name == "" {
+		file = &renamedFile{File: file, name: name}
+	}
+	return &fileFS{File: file}
 }
 
 type fileFS struct {
-	file fs.File
-	name string
+	fs.File
 }
-
-var _ fs.FS = (*fileFS)(nil)
-var _ fs.OpenContextFS = (*fileFS)(nil)
-var _ fs.StatFS = (*fileFS)(nil)
 
 func (f *fileFS) Open(name string) (iofs.File, error) {
 	return f.OpenContext(context.Background(), name)
 }
 
-func (f *fileFS) Stat(name string) (iofs.FileInfo, error) {
-	if name == "." {
-		fi, err := f.file.Stat()
-		if err != nil {
-			return Entry(f.name, 0644), nil
-		}
-		return RawNode(fi, f.name), nil
-	}
-	return nil, &fs.PathError{Op: "stat", Path: name, Err: fs.ErrNotExist}
-}
-
 func (f *fileFS) OpenContext(ctx context.Context, name string) (iofs.File, error) {
-	// Only "." is supported and maps to the file itself
 	if name == "." {
-		return &renamedFile{File: f.file, name: f.name}, nil
+		return f.File, nil
 	}
 	return nil, &fs.PathError{Op: "open", Path: name, Err: fs.ErrNotExist}
 }
