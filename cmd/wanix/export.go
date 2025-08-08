69a817cc63f1620c8f434d54b562f81ef0914211
Jeff Lindsay <progrium@gmail.com>
2025-08-08 16:01:45 -0700

    cmd: update export to be used to deploy wanix.run for #187

--- cmd/wanix/export.go
+++ cmd/wanix/export.go
@@ -3,16 +3,13 @@
 package main
 
 import (
-	"archive/tar"
-	"bytes"
-	"io"
 	"log"
 	"os"
+	"path/filepath"
 
 	"tractor.dev/toolkit-go/engine/cli"
 	"tractor.dev/wanix/external/linux"
 	v86 "tractor.dev/wanix/external/v86"
-	"tractor.dev/wanix/fs"
 	"tractor.dev/wanix/fs/fskit"
 	"tractor.dev/wanix/runtime/assets"
 	"tractor.dev/wanix/shell"
@@ -20,10 +17,35 @@ import (
 
 func exportCmd() *cli.Command {
 	cmd := &cli.Command{
-		Usage: "export",
+		Usage: "export <dir>",
 		Short: "(deprecated)",
+		Args:  cli.ExactArgs(1),
 		Run: func(ctx *cli.Context, args []string) {
 			log.SetFlags(log.Ltime | log.Lmicroseconds | log.Lshortfile)
+
+			dir := args[0]
+
+			// Check if dir exists
+			if fi, err := os.Stat(dir); err == nil {
+				if !fi.IsDir() {
+					log.Fatalf("%s exists but is not a directory", dir)
+				}
+				// Check if directory is empty
+				entries, err := os.ReadDir(dir)
+				if err != nil {
+					log.Fatal(err)
+				}
+				if len(entries) > 0 {
+					log.Fatalf("Directory %s is not empty", dir)
+				}
+			} else if os.IsNotExist(err) {
+				// Create directory if it doesn't exist
+				if err := os.MkdirAll(dir, 0755); err != nil {
+					log.Fatal(err)
+				}
+			} else {
+				log.Fatal(err)
+			}
 
 			fsys := fskit.UnionFS{assets.Dir, fskit.MapFS{
 				"v86":   v86.Dir,
@@ -37,59 +59,35 @@ func exportCmd() *cli.Command {
 				log.Fatal(err)
 			}
 
-			// Create a new tar writer
-			var buf bytes.Buffer
-			tarWriter := tar.NewWriter(&buf)
+			fatal(os.MkdirAll(filepath.Join(dir, "v86"), 0755))
+			fatal(os.MkdirAll(filepath.Join(dir, "wasi"), 0755))
+			fatal(os.MkdirAll(filepath.Join(dir, "shell"), 0755))
+			fatal(os.MkdirAll(filepath.Join(dir, "linux"), 0755))
 
-			fatal(addFileToTar(tarWriter, wasmFsys, "wanix.wasm"))
-			fatal(addFileToTar(tarWriter, fsys, "wanix.min.js"))
-			fatal(addFileToTar(tarWriter, fsys, "wanix-sw.js"))
-			fatal(addFileToTar(tarWriter, fsys, "wanix.css"))
-			fatal(addFileToTar(tarWriter, fsys, "favicon.ico"))
-			fatal(addFileToTar(tarWriter, fsys, "wasi/worker.js"))
-			fatal(addFileToTar(tarWriter, fsys, "wasi/worker_sync.js"))
-			fatal(addFileToTar(tarWriter, fsys, "shell/shell.tgz"))
-			fatal(addFileToTar(tarWriter, fsys, "linux/bzImage"))
-			fatal(addFileToTar(tarWriter, fsys, "v86/v86.wasm"))
-			fatal(addFileToTar(tarWriter, fsys, "v86/seabios.bin"))
-			fatal(addFileToTar(tarWriter, fsys, "v86/vgabios.bin"))
-			fatal(addFileToTar(tarWriter, fsys, "index.html"))
+			// Copy files to directory
+			fatal(copyFile(wasmFsys, "wanix.wasm", filepath.Join(dir, "wanix.wasm")))
+			for _, f := range []string{
+				"wanix.min.js",
+				"wanix-sw.js",
+				"wanix.css",
+				"favicon.ico",
+				"wasi/lib.js",
+				"wasi/worker.js",
+				"wasi/worker_sync.js",
+				"shell/shell.tgz",
+				"linux/bzImage",
+				"v86/libv86.js",
+				"v86/v86.wasm",
+				"v86/seabios.bin",
+				"v86/vgabios.bin",
+				"index.html",
+			} {
+				if err := copyFile(fsys, f, filepath.Join(dir, f)); err != nil {
+					log.Fatal(err)
+				}
+			}
 
-			tarWriter.Close()
-			os.Stdout.Write(buf.Bytes())
 		},
 	}
 	return cmd
-}
-
-func addFileToTar(tarWriter *tar.Writer, fsys fs.FS, name string) error {
-	// Open the file
-	file, err := fsys.Open(name)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	// Get file information for header
-	fileInfo, err := file.Stat()
-	if err != nil {
-		return err
-	}
-
-	// Create a tar header from the file info
-	header, err := tar.FileInfoHeader(fileInfo, "")
-	if err != nil {
-		return err
-	}
-
-	header.Name = name
-
-	// Write the header to the tar archive
-	if err := tarWriter.WriteHeader(header); err != nil {
-		return err
-	}
-
-	// Copy the file contents to the tar writer
-	_, err = io.Copy(tarWriter, file)
-	return err
 }
