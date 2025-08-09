8756bb057ac75f97b3ac61599f0e9d2be13e8a82
Jeff Lindsay <progrium@gmail.com>
2025-08-08 18:48:25 -0700

    runtime: add showScreen and hideScreen helpers, makeScreen hides by default

--- runtime/helpers.js
+++ runtime/helpers.js
@@ -39,27 +39,25 @@ export function setupConsoleHelpers() {
     };
 
 
-    // window.makeScreen = () => {
-    //     const screen = document.createElement('div');
-    //     const div = document.createElement('div');
-    //     const canvas = document.createElement('canvas');
-    //     screen.appendChild(div);
-    //     screen.appendChild(canvas);
-    //     screen.id = 'screen';
-    //     document.body.appendChild(screen);
-    // };
+    window.makeScreen = () => {
+        const screen = document.createElement('div');
+        const div = document.createElement('div');
+        const canvas = document.createElement('canvas');
+        screen.appendChild(div);
+        screen.appendChild(canvas);
+        screen.id = 'screen';
+        screen.style.display = 'none';
+        document.body.appendChild(screen);
+    };
 
-    // window.bootAlpine = async (screen=false) => {
-    //     if (screen) {
-    //         makeScreen();
-    //     }
-    //     const w = window.wanix.instance;
-    //     const vid = (await w.readText("web/vm/new")).trim();
-    //     await w.writeFile("task/1/ctl", `bind #console/data1 web/vm/${vid}/ttyS0`);
-    //     await w.writeFile("task/1/ctl", `bind #alpine web/vm/${vid}/fsys`);
-    //     await w.writeFile("task/1/ctl", `bind web/opfs/init web/vm/${vid}/fsys/bin/init`);
-    //     await w.writeFile("task/1/ctl", `bind web/opfs/bin/post-dhcp web/vm/${vid}/fsys/bin/post-dhcp`);
-    //     // await w.writeFile("task/1/ctl", "bind . web/vm/2/fsys");
-    //     await w.writeFile(`web/vm/${vid}/ctl`, "start");
-    // }
+    window.showScreen = () => {
+        const screen = document.getElementById('screen');
+        screen.style.display = 'block';
+    };
+
+    window.hideScreen = () => {
+        const screen = document.getElementById('screen');
+        screen.style.display = 'none';
+    };
+
 }
