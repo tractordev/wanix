9965cc41c0a43d0bf5e803654d034ae4d4f233a8
Jeff Lindsay <progrium@gmail.com>
2025-08-08 22:33:28 -0700

    runtime: update wasm_exec in wasi worker, add support for WASIProcExit, and consolidate into single worker JS file

--- runtime/wasi/mod.ts
+++ runtime/wasi/mod.ts
@@ -2,7 +2,7 @@ export { CallBuffer } from "./callbuffer.ts";
 export { FileHandle, DirectoryHandle } from "./wanix.ts";
 export { OpenFile, File, Directory, PreopenDirectory } from "./fs.ts";
 export { EmptyFile, OpenEmptyFile } from "./empty.ts";
-export { WASI, ConsoleStdout } from "@bjorn3/browser_wasi_shim";
+export { WASI, WASIProcExit, ConsoleStdout } from "@bjorn3/browser_wasi_shim";
 export { applyPatchPollOneoff } from "./poll-oneoff.ts";
 
 export { WanixFS } from "../fs.js";
