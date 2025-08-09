2cbae9d5db021a5dcfbf8eb5293e0f728c55ae82
Jeff Lindsay <progrium@gmail.com>
2025-08-08 17:31:10 -0700

    linux: add kernel modules for containers

--- web/vm/v86.go
+++ web/vm/v86.go
@@ -11,8 +11,8 @@ func makeVM(id string) js.Value {
 	options := map[string]any{
 		"wasm_path":        "./v86/v86.wasm",
 		"screen_container": js.Global().Get("document").Call("getElementById", "screen"),
-		"memory_size":      512 * 1024 * 1024,
-		"vga_memory_size":  8 * 1024 * 1024,
+		"memory_size":      512 * 1024 * 1024, // 512MB
+		"vga_memory_size":  8 * 1024 * 1024,   // 8MB
 		"net_device": map[string]any{
 			"type":      "ne2k",
 			"relay_url": "ws://localhost:8777",
@@ -26,7 +26,7 @@ func makeVM(id string) js.Value {
 		"bzimage": map[string]any{
 			"url": "./linux/bzImage",
 		},
-		"cmdline": fmt.Sprintf("init=/bin/init rw root=host9p rootfstype=9p rootflags=trans=virtio,version=9p2000.L,aname=web/vm/%s/fsys,cache=none,msize=8192,access=client tsc=reliable mitigations=off random.trust_cpu=on", id),
+		"cmdline": fmt.Sprintf("init=/bin/init rw root=host9p rootfstype=9p rootflags=trans=virtio,version=9p2000.L,aname=web/vm/%s/fsys,cache=none,msize=8192,access=client tsc=reliable mitigations=off random.trust_cpu=on ramdisk_size=102400", id),
 	}
 	vm := js.Global().Get("V86").New(options)
 	readyPromise := js.Global().Get("Promise").New(js.FuncOf(func(this js.Value, args []js.Value) interface{} {
