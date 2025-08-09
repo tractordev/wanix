341a00aa07411c26fe7a77bf8de6fbb82f0e3a55
Jeff Lindsay <progrium@gmail.com>
2025-08-08 22:31:32 -0700

    test: add tinygo check

---
+++ test/wasi/tinygo/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+)
+
+func main() {
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
+
+	wd, _ := os.Getwd()
+	fmt.Fprintf(w, "Dir:\t%s\n", wd)
+
+	fmt.Fprintf(w, "Args:\t%v\n", os.Args)
+
+	fmt.Fprintf(w, "Env:\t%v\n", os.Environ())
+
+	entries, _ := os.ReadDir("/")
+	var root []string
+	for _, entry := range entries {
+		if entry.IsDir() {
+			root = append(root, entry.Name()+"/")
+		} else {
+			root = append(root, entry.Name())
+		}
+	}
+	fmt.Fprintf(w, "Root:\t%v\n", root)
+
+	w.Flush()
+}
