96e64304a629aa3ab7c100bb0f63e23342140d10
Jeff Lindsay <progrium@gmail.com>
2025-08-27 12:52:08 -0700

    shell: manually add in kernel devices

--- shell/init.js
+++ shell/init.js
@@ -5,6 +5,9 @@
     const vm = (await w.readText("vm/new/default")).trim();
     await w.writeFile("task/1/ctl", `bind #console/data1 vm/${vm}/ttyS0`);
     await w.writeFile("task/1/ctl", `bind . vm/${vm}/fsys`);
+    await w.writeFile("task/1/ctl", `bind #ramfs vm/${vm}/fsys/#ramfs`);
+    await w.writeFile("task/1/ctl", `bind #pipe vm/${vm}/fsys/#pipe`);
+    await w.writeFile("task/1/ctl", `bind #| vm/${vm}/fsys/#|`);
     await w.writeFile("task/1/ctl", `bind #bundle/rootfs vm/${vm}/fsys`);
     await w.writeFile(`vm/${vm}/ctl`, `start -append 'init=/bin/init rw root=host9p rootfstype=9p rootflags=trans=virtio,version=9p2000.L,aname=vm/${vm}/fsys,cache=none,msize=8192,access=client ramdisk_size=102400'`);
 })();
