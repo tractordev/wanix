bbb473293cadc1f54d56fa3cf5fd03e5942fced1
Jeff Lindsay <progrium@gmail.com>
2025-08-27 18:24:44 -0700

    fs: fix rename (finally)

--- fs/rename.go
+++ fs/rename.go
@@ -1,5 +1,10 @@
 package fs
 
+import (
+	"log"
+	"path"
+)
+
 type RenameFS interface {
 	FS
 	Rename(oldname, newname string) error
@@ -20,15 +25,16 @@ func Rename(fsys FS, oldname, newname string) error {
 		return opErr(fsys, newname, "rename", err)
 	}
 
-	newfsys, newrname, err := ResolveTo[RenameFS](fsys, ContextFor(fsys), newname)
+	newfsys, newrname, err := ResolveTo[RenameFS](fsys, ContextFor(fsys), path.Dir(newname))
 	if err != nil {
 		return opErr(fsys, newname, "rename", err)
 	}
 
 	if Equal(oldfsys, newfsys) {
-		return oldfsys.Rename(oldrname, newrname)
+		return oldfsys.Rename(oldrname, path.Join(newrname, path.Base(newname)))
 	}
 
+	log.Println("rename across filesystems not implemented")
 	// TODO:
 	// - potentially use CopyFS and RemoveAll to move as last resort
 
