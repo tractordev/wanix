c15cc8edfdb8dbe93788c71cf6ac56eab7b9fcbf
Jeff Lindsay <progrium@gmail.com>
2025-08-27 18:21:38 -0700

    alpine: update build and init

--- hack/alpine/init.js
+++ hack/alpine/init.js
@@ -6,5 +6,5 @@
     await w.writeFile("task/1/ctl", `bind #console/data1 vm/${vm}/ttyS0`);
     await w.writeFile("task/1/ctl", `bind . vm/${vm}/fsys`);
     await w.writeFile("task/1/ctl", `bind #bundle/rootfs vm/${vm}/fsys`);
-    await w.writeFile(`vm/${vm}/ctl`, "start -append 'init=/bin/init rw root=host9p rootfstype=9p rootflags=trans=virtio,version=9p2000.L,aname=vm/${vm}/fsys,cache=none,msize=8192,access=client ramdisk_size=102400'");
+    await w.writeFile(`vm/${vm}/ctl`, `start -append 'init=/bin/init rw root=host9p rootfstype=9p rootflags=trans=virtio,version=9p2000.L,aname=vm/${vm}/fsys,cache=none,msize=8192,access=client ramdisk_size=102400'`);
 })();
