0213c29550d40c437ca56fb40cb1b5483c6d5edb
Jeff Lindsay <progrium@gmail.com>
2025-08-27 18:26:18 -0700

    fusekit: replace syscall constants with os constants (best practice)

--- fs/fusekit/util.go
+++ fs/fusekit/util.go
@@ -3,6 +3,7 @@ package fusekit
 import (
 	"hash/fnv"
 	iofs "io/fs"
+	"os"
 	"syscall"
 
 	"github.com/hanwen/go-fuse/v2/fuse"
@@ -28,28 +29,28 @@ func applyStat(out *fuse.Attr, fi iofs.FileInfo) {
 
 func openFlags(flags uint32) []string {
 	var flagStrs []string
-	if flags&syscall.O_RDONLY != 0 {
+	if flags&os.O_RDONLY != 0 {
 		flagStrs = append(flagStrs, "O_RDONLY")
 	}
-	if flags&syscall.O_WRONLY != 0 {
+	if flags&os.O_WRONLY != 0 {
 		flagStrs = append(flagStrs, "O_WRONLY")
 	}
-	if flags&syscall.O_RDWR != 0 {
+	if flags&os.O_RDWR != 0 {
 		flagStrs = append(flagStrs, "O_RDWR")
 	}
-	if flags&syscall.O_APPEND != 0 {
+	if flags&os.O_APPEND != 0 {
 		flagStrs = append(flagStrs, "O_APPEND")
 	}
-	if flags&syscall.O_CREAT != 0 {
+	if flags&os.O_CREATE != 0 {
 		flagStrs = append(flagStrs, "O_CREAT")
 	}
-	if flags&syscall.O_EXCL != 0 {
+	if flags&os.O_EXCL != 0 {
 		flagStrs = append(flagStrs, "O_EXCL")
 	}
-	if flags&syscall.O_SYNC != 0 {
+	if flags&os.O_SYNC != 0 {
 		flagStrs = append(flagStrs, "O_SYNC")
 	}
-	if flags&syscall.O_TRUNC != 0 {
+	if flags&os.O_TRUNC != 0 {
 		flagStrs = append(flagStrs, "O_TRUNC")
 	}
 	return flagStrs
