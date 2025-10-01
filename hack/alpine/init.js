59341320c0f283c02d4f5595f918329b36f3cd97
Jeff Lindsay <progrium@gmail.com>
2025-09-30 20:49:53 -0500

    alpine: new init.js format, ensure arch for v86 image, add 9p caching

--- hack/alpine/init.js
+++ hack/alpine/init.js
@@ -1,7 +1,6 @@
-(async () => {
+export default async (w) => {
     console.log("running alpine init.js");
     window.makeScreen();
-    const w = window.wanix.instance;        
     const vm = (await w.readText("vm/new/default")).trim();
     await w.writeFile("task/1/ctl", `bind #console/data1 vm/${vm}/ttyS0`);
     await w.writeFile("task/1/ctl", `bind . vm/${vm}/fsys`);
@@ -11,12 +10,12 @@
         "rw",
         "root=host9p",
         "rootfstype=9p",
-        `rootflags=trans=virtio,version=9p2000.L,aname=vm/${vm}/fsys,cache=none`,
+        `rootflags=trans=virtio,version=9p2000.L,aname=vm/${vm}/fsys,cache=loose`, //msize=524288
     ];
-    const ctlcmd = ["start", "-append", `'${cmdline.join(" ")}'`];
-    if (window.wanix.config.network) {
+    const ctlcmd = ["start", "-m", "1024M", "-append", `'${cmdline.join(" ")}'`];
+    if (w.config.network) {
         ctlcmd.push("-netdev");
-        ctlcmd.push(`user,type=virtio,relay_url=${window.wanix.config.network}`);
+        ctlcmd.push(`user,type=virtio,relay_url=${w.config.network}`);
     }
     await w.writeFile(`vm/${vm}/ctl`, ctlcmd.join(" "));
-})();
+}
