d9345be6d8e3686a020c87fe7ff23f30ac7e8456
Jeff Lindsay <progrium@gmail.com>
2025-09-30 20:50:25 -0500

    shell: update init.js format, ensure arch for v86 image

--- shell/init.js
+++ shell/init.js
@@ -1,7 +1,6 @@
-(async () => {
+export default async (w) => {
     console.log("running init.js");
     window.makeScreen();
-    const w = window.wanix.instance;        
     const vm = (await w.readText("vm/new/default")).trim();
     await w.writeFile("task/1/ctl", `bind #console/data1 vm/${vm}/ttyS0`);
     await w.writeFile("task/1/ctl", `bind . vm/${vm}/fsys`);
@@ -10,4 +9,4 @@
     await w.writeFile("task/1/ctl", `bind #| vm/${vm}/fsys/#|`);
     await w.writeFile("task/1/ctl", `bind #bundle/rootfs vm/${vm}/fsys`);
     await w.writeFile(`vm/${vm}/ctl`, `start -append 'init=/bin/init rw root=host9p rootfstype=9p rootflags=trans=virtio,version=9p2000.L,aname=vm/${vm}/fsys,cache=none,msize=8192,access=client ramdisk_size=102400'`);
-})();
+}
