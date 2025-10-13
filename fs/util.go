b9ea62b879595ed21e4a98aef736712e24b17f78
Jeff Lindsay <progrium@gmail.com>
2025-10-13 16:29:12 -0400

    fs: drop WriteFile since it lives in its own file now

--- fs/util.go
+++ fs/util.go
@@ -1,7 +1,6 @@
 package fs
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -60,32 +59,6 @@ func DirExists(fsys FS, path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
-}
-
-func WriteFile(fsys FS, filename string, data []byte, perm FileMode) error {
-	f, err := Create(fsys, filename)
-	if errors.Is(err, ErrNotSupported) {
-		var e error
-		f, e = fsys.Open(filename)
-		if errors.Is(e, ErrNotExist) {
-			// ok go back to unsupported error
-			return err //fmt.Errorf("create: %w on %s", ErrNotSupported, reflect.TypeOf(fsys))
-		}
-		if e != nil {
-			return e
-		}
-	} else if err != nil {
-		return err
-	}
-	n, err := Write(f, data)
-	if err == nil && n < len(data) {
-		err = io.ErrShortWrite
-	}
-	if err1 := f.Close(); err == nil {
-		err = err1
-	}
-	// TODO: use perm?
-	return err
 }
 
 func AppendFile(fsys FS, filename string, data []byte) error {
