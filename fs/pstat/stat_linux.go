a7ca82ce09962afbfc5c63047187681ecb3e392c
Jeff Lindsay <progrium@gmail.com>
2025-10-14 22:17:43 -0400

    pstat: fix types for linux and use explicit darwin tagged source

---
+++ fs/pstat/stat_linux.go
@@ -0,0 +1,48 @@
+//go:build linux || dragonfly || solaris
+// +build linux dragonfly solaris
+
+package pstat
+
+import (
+	"syscall"
+)
+
+func SysToStat(sys any) *Stat {
+	if sys == nil {
+		return &Stat{}
+	}
+	nativeStat := sys.(*syscall.Stat_t)
+	return &Stat{
+		Dev:     nativeStat.Dev,
+		Ino:     nativeStat.Ino,
+		Nlink:   uint64(nativeStat.Nlink),
+		Mode:    nativeStat.Mode,
+		Uid:     nativeStat.Uid,
+		Gid:     nativeStat.Gid,
+		Rdev:    nativeStat.Rdev,
+		Size:    nativeStat.Size,
+		Blksize: int64(nativeStat.Blksize),
+		Blocks:  nativeStat.Blocks,
+		Atim:    NsecToTimespec(syscall.TimespecToNsec(nativeStat.Atim)),
+		Mtim:    NsecToTimespec(syscall.TimespecToNsec(nativeStat.Mtim)),
+		Ctim:    NsecToTimespec(syscall.TimespecToNsec(nativeStat.Ctim)),
+	}
+}
+
+func StatToSys(stat *Stat) any {
+	return &syscall.Stat_t{
+		Dev:     stat.Dev,
+		Ino:     stat.Ino,
+		Nlink:   uint32(stat.Nlink),
+		Mode:    stat.Mode,
+		Uid:     stat.Uid,
+		Gid:     stat.Gid,
+		Rdev:    stat.Rdev,
+		Size:    stat.Size,
+		Blksize: int32(stat.Blksize),
+		Blocks:  stat.Blocks,
+		Atim:    syscall.NsecToTimespec(stat.Atim.Nsec),
+		Mtim:    syscall.NsecToTimespec(stat.Mtim.Nsec),
+		Ctim:    syscall.NsecToTimespec(stat.Ctim.Nsec),
+	}
+}
