e1b5dce016417328a5523da739c630b2094f8a84
Jeff Lindsay <progrium@gmail.com>
2025-10-20 19:05:04 -0400

    runtime: async iterator polyfill for ReadableStream on safari

--- runtime/handle.js
+++ runtime/handle.js
@@ -135,3 +135,19 @@ export class WanixHandle {
         });
     }
 }
+
+// for safari
+if (!ReadableStream.prototype[Symbol.asyncIterator]) {
+    ReadableStream.prototype[Symbol.asyncIterator] = async function* () {
+        const reader = this.getReader();
+        try {
+            while (true) {
+                const { done, value } = await reader.read();
+                if (done) return;
+                yield value;
+            }
+        } finally {
+            reader.releaseLock();
+        }
+    };
+}
