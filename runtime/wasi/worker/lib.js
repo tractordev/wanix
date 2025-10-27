ad31454980514232fca901fdce43eb0b59c3f462
Jeff Lindsay <progrium@gmail.com>
2025-10-27 18:02:30 -0400

    wasi: add async iterator polyfill for safari

--- runtime/wasi/worker/lib.js
+++ runtime/wasi/worker/lib.js
@@ -8478,6 +8478,20 @@ var WanixHandle2 = class {
     });
   }
 };
+if (!ReadableStream.prototype[Symbol.asyncIterator]) {
+  ReadableStream.prototype[Symbol.asyncIterator] = async function* () {
+    const reader = this.getReader();
+    try {
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) return;
+        yield value;
+      }
+    } finally {
+      reader.releaseLock();
+    }
+  };
+}
 export {
   CallBuffer,
   ConsoleStdout,
