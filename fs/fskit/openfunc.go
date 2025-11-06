123520cd2307b090a48cdae92aed40d1b04e84da
Jeff Lindsay <progrium@gmail.com>
2025-11-06 11:59:19 -0800

    fskit: OpenFunc should probably support OpenFile

--- fs/fskit/openfunc.go
+++ fs/fskit/openfunc.go
@@ -14,3 +14,7 @@ func (f OpenFunc) Open(name string) (fs.File, error) {
 func (f OpenFunc) OpenContext(ctx context.Context, name string) (fs.File, error) {
 	return f(ctx, name)
 }
+
+func (f OpenFunc) OpenFile(name string, flag int, perm fs.FileMode) (fs.File, error) {
+	return f(context.Background(), name)
+}
