7b9533e08e0984f6afd109849a5644dce70663e4
Jeff Lindsay <progrium@gmail.com>
2026-04-29 15:17:51 -0700

    examples: add basic-workbench example. cleanup.

--- examples/serve.go
+++ examples/serve.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	log.Println("serving examples at http://localhost:7070/examples")
 	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		// Enable cross-origin isolation, needed for SharedArrayBuffer/WebAssembly threading/etc.
 		w.Header().Set("Cross-Origin-Opener-Policy", "same-origin")
 		w.Header().Set("Cross-Origin-Embedder-Policy", "require-corp")
