483e4c0ddb5834dcb31f643b70e93b14a3fe0467
Jeff Lindsay <progrium@gmail.com>
2026-07-02 19:13:59 -0700

    fs: add OpenFileContext to OpenFunc, as well as no-op chmod (for now i guess?)

--- fs/fskit/openfunc.go
+++ fs/fskit/openfunc.go
@@ -2,10 +2,13 @@ package fskit
 
 import (
 	"context"
-	"io/fs"
+
+	"tractor.dev/wanix/fs"
 )
 
 type OpenFunc func(ctx context.Context, name string) (fs.File, error)
+
+var _ fs.OpenFileContextFS = OpenFunc(nil)
 
 func (f OpenFunc) Open(name string) (fs.File, error) {
 	return f(context.Background(), name)
@@ -17,4 +20,13 @@ func (f OpenFunc) OpenContext(ctx context.Context, name string) (fs.File, error)
 
 func (f OpenFunc) OpenFile(name string, flag int, perm fs.FileMode) (fs.File, error) {
 	return f(context.Background(), name)
+}
+
+func (f OpenFunc) OpenFileContext(ctx context.Context, name string, flag int, perm fs.FileMode) (fs.File, error) {
+	return f(ctx, name)
+}
+
+// todo: this shouldn't be needed, but otherwise >> appends to files fail
+func (f OpenFunc) Chmod(name string, mode fs.FileMode) (err error) {
+	return nil
 }
