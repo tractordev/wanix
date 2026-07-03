3d40569dcfcec4cb81bab88df8f79c80fc89a20d
Jeff Lindsay <progrium@gmail.com>
2026-07-03 15:33:47 -0700

    jsutil: better cross runtime error support

--- misc/jsutil/await.go
+++ misc/jsutil/await.go
@@ -2,7 +2,10 @@
 
 package jsutil
 
-import "syscall/js"
+import (
+	"fmt"
+	"syscall/js"
+)
 
 func Await(promise js.Value) js.Value {
 	ch := make(chan js.Value, 1)
@@ -11,14 +14,14 @@ func Await(promise js.Value) js.Value {
 		return nil
 	})
 	defer resolveFn.Release()
-	
+
 	promise.Call("then", resolveFn)
 	return <-ch
 }
 
 func AwaitErr(promise js.Value) (js.Value, error) {
 	ch := make(chan js.Value, 2)
-	
+
 	// Create closures that will be released after promise settles
 	resolveFn := js.FuncOf(func(this js.Value, args []js.Value) any {
 		ch <- args[0] // resolve
@@ -30,17 +33,35 @@ func AwaitErr(promise js.Value) (js.Value, error) {
 		ch <- args[0] // reject
 		return nil
 	})
-	
+
 	// Release after promise settles
 	defer resolveFn.Release()
 	defer rejectFn.Release()
-	
+
 	promise.Call("then", resolveFn, rejectFn)
-	
+
 	resolved := <-ch
 	rejected := <-ch
 	if rejected.Truthy() {
-		return js.Undefined(), js.Error{Value: rejected}
+		return js.Undefined(), jsError(rejected)
 	}
 	return resolved, nil
+}
+
+// jsError converts a JS rejection value to a Go error.
+// Duplex and other JS code often reject with a plain string; wrapping that in
+// js.Error panics because Error() calls Value.Get("message") on a string.
+func jsError(v js.Value) error {
+	switch v.Type() {
+	case js.TypeString:
+		return fmt.Errorf("%s", v.String())
+	case js.TypeObject:
+		msg := v.Get("message")
+		if msg.Truthy() && msg.Type() == js.TypeString {
+			return fmt.Errorf("%s", msg.String())
+		}
+		return js.Error{Value: v}
+	default:
+		return fmt.Errorf("%v", v)
+	}
 }
